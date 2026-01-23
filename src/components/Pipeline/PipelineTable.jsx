c90c69d5f9ed628e0c3ef9497c8626b5e9cd5713
OlawumiPT <OOladimeji@power-transitions.com>
2026-01-23 17:15:55 -0600

    Update PipelineTable.jsx

--- src/components/Pipeline/PipelineTable.jsx
+++ src/components/Pipeline/PipelineTable.jsx
@@ -918,13 +918,6 @@ const PipelineTable = ({
                           <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
                         </svg>
                       </button>
-                      <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                          <path d="M3 6h18" />
-                          <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
-                          <path d="M10 11v6" />
-                          <path d="M14 11v6" />
-                        </svg>
-                      </button>
                     </div>
                   </td>
                 </tr>
