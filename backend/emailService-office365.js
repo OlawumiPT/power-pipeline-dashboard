a12d24ff73c86825efa37a36903d91be1dfc85ee
OlawumiPT <OOladimeji@power-transitions.com>
2026-01-23 13:30:29 -0600

    Update emailService-office365.js

--- backend/emailService-office365.js
+++ backend/emailService-office365.js
@@ -611,6 +611,7 @@ class EmailService {
   }
 
   async sendAdminNotification(userData, approvalToken) {
+    
     // const approvalLink = `${process.env.FRONTEND_URL || 'http://localhost:5173'}/admin/approve/${approvalToken}?user=${encodeURIComponent(userData.username)}`;
   const approvalLink = `${process.env.BACKEND_URL || 'https://pt-power-pipeline-api.azurewebsites.net'}/api/admin/approve/${approvalToken}`;
     return this.sendEmail(
